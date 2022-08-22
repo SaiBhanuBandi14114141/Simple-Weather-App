a13bf2da9005a42038c7b14bfb8dadbc5f2545ea
SaiBhanuBandi14114141 <105918956+SaiBhanuBandi14114141@users.noreply.github.com>
2022-08-22 19:27:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Weather from './Weather'
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <h2>
+          <Weather/>
+        </h2>
+      </center>
+    </div>
+  )
+}
+
+export default App
